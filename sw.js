9f27861aa7062b050acd33ccef42a60baa6f46b9
Gaby Franco <109766180+gabyfrancom@users.noreply.github.com>
2026-09-12 15:19:42 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'volver-a-mi-v1';
+const ARCHIVOS = ['./', './index.html', './data.js', './manifest.json',
+  './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k =>
+    Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
